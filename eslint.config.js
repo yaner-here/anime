27dc04b00ef874cf71379a5eb6e69ba438923957
yaner-here <1468275133@qq.com>
2024-12-05 01:31:25 +0800

    chore: disable eslint's rule of "react/react-in-jsx-scope"

--- eslint.config.js
+++ eslint.config.js
@@ -9,5 +9,10 @@ export default [
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
+    {
+        ...pluginReact.configs.flat.recommended,
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+        },
+    },
 ];
