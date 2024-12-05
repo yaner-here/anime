374cb8f4663174e43f99b14411657f127222874d
yaner-here <1468275133@qq.com>
2024-12-05 23:25:34 +0800

    Merge branch 'main' into tldraw

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
+import MillionLint from '@million/lint';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), MillionLint.vite()],
 });
