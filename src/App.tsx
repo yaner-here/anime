780666e57a09a6114f119366aa2958479612d0c9
yaner-here <1468275133@qq.com>
2024-12-05 23:23:28 +0800

    feat: Add Tldraw interface with customed font

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,32 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Editor, Tldraw } from 'tldraw';
+import 'tldraw/tldraw.css';
+
 import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0);
-
+    const handleMount = (editor: Editor) => {
+        editor.createShape({
+            type: 'text',
+            x: 200,
+            y: 200,
+            props: {
+                text: 'Hello, world!',
+            },
+        });
+        alert('Welcome. handleMount() is called.');
+    };
     return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <div style={{ position: 'fixed', inset: 0 }}>
+            <Tldraw
+                persistenceKey="main"
+                onMount={handleMount}
+                assetUrls={{
+                    fonts: {
+                        monospace: 'src/assets/fonts/CascadiaCodeNF.ttf',
+                    },
+                }}
+            />
+        </div>
     );
 }
 
