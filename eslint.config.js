eca63b9cdd262266e32f478439a2bd3136249eb9
Yaner <1468275133@qq.com>
2024-12-06 13:58:55 +0800

    Init: enable eslint extension for react

--- eslint.config.js
+++ eslint.config.js
@@ -2,17 +2,31 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import react from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
     { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
     {
         ...pluginReact.configs.flat.recommended,
         rules: {
             'react/react-in-jsx-scope': 'off',
+        },
+    },
+    {
+        settings: {
+            react: {
+                version: '18.3',
+            },
+        },
+        plugins: { react },
+        rules: {
+            ...react.configs.recommended.rules,
+            ...react.configs['jsx-runtime'].rules,
         },
     },
 ];
