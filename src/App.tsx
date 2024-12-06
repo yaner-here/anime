86b9c60a17929804b5faab3d9a4cb79cb9227ea9
yaner-here <1468275133@qq.com>
2024-12-07 01:48:21 +0800

    feat: use toolpad built-in router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
 import './App.css';
 import { AppProvider, DashboardLayout } from '@toolpad/core';
 import { Navigation } from '@toolpad/core';
+import { Box, Typography } from '@mui/material';
+import { useDemoRouter } from '@toolpad/core/internal';
+import { Home, Settings } from '@mui/icons-material';
 
 const navigation: Navigation = [
     {
         segment: 'home',
         title: 'Home',
-        // icon: <HomeIcon />
+        icon: <Home />,
+    },
+    {
+        segment: 'setting',
+        title: 'Setting',
+        icon: <Settings />,
     },
 ];
 
 function App() {
+    const router = useDemoRouter('/home');
     return (
-        <AppProvider navigation={navigation}>
-            <DashboardLayout children={undefined}></DashboardLayout>
+        <AppProvider
+            navigation={navigation}
+            branding={{
+                logo: <img src="https://mui.com/static/logo.png" />,
+                title: 'Panel',
+            }}
+            router={router}
+        >
+            <DashboardLayout>
+                <Box>
+                    <Typography>{router.pathname}</Typography>
+                </Box>
+            </DashboardLayout>
         </AppProvider>
     );
 }
