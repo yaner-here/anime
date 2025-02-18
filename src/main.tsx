a2543c24d0effde5f69cdbcb90659a6af16978a2
Yaner <1468275133@qq.com>
2025-02-18 21:09:26 +0800

    init: intstall react-scan

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
+import { scan } from 'react-scan'; // must be imported before react
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import './index.css';
 import App from './App.tsx';
+
+scan({ enabled: true }); // react-scan profiler
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
